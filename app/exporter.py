b1fa65f2ea99422e7fe8b5d15723f5ed3b0c607e
Thirunavukkarasu Ravichandran <thirunavukkarasu.ravichandran@webtrekk.com>
2018-10-07 14:28:48 +0200

    generic info metrics

--- app/exporter.py
+++ app/exporter.py
@@ -10,41 +10,46 @@ import yaml
 from objectpath import Tree
 import logging
 
-DEFAULT_PORT=9158
-DEFAULT_LOG_LEVEL='info'
+DEFAULT_PORT = 9158
+DEFAULT_LOG_LEVEL = 'info'
+
 
 class JsonPathCollector(object):
-  def __init__(self, config):
-    self._config = config
+    def __init__(self, config):
+        self._config = config
 
-  def collect(self):
-    config = self._config
-    endpoints = config['json_data_urls']
-    for endpoint in endpoints:
-      result = json.loads(urllib2.urlopen(endpoint['url'], timeout=10).read())
-      result_tree = Tree(result)
-      for metric_config in config['metrics']:
-        metric_name = metric_config['name']
-        metric_description = metric_config.get('description', '')
-        metric_path = metric_config['path']
-        value = result_tree.execute(metric_path)
-        logging.debug("metric_name: {}, value for '{}' : {}".format(metric_name, metric_path, value))
-        metric = GaugeMetricFamily(metric_name, metric_description, labels=[endpoint['label']])
-        metric.add_metric([value], 1)
-        yield metric
+    def collect(self):
+        config = self._config
+        endpoints = config['json_data_urls']
+        for endpoint in endpoints:
+            result = json.loads(urllib2.urlopen(endpoint['url'], timeout=10).read())
+            result_tree = Tree(result)
+            labels = ['instance_id']
+            values = [endpoint['label']]
+            for tag in config['tags']:
+                tag_name = tag['name']
+                tag_path = tag['path']
+                value = result_tree.execute(tag_path)
+                logging.info("tag_name: {}, value for '{}' : {}".format(tag_name, tag_path, value))
+                labels.append(tag_name)
+                values.append(value)
+            metric = GaugeMetricFamily(config['metric_name'], "spring boot info", labels=labels)
+            metric.add_metric(values, 1)
+            yield metric
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(description='Expose metrics bu jsonpath for configured url')
-  parser.add_argument('config_file_path', help='Path of the config file')
-  args = parser.parse_args()
-  with open(args.config_file_path) as config_file:
-    config = yaml.load(config_file)
-    log_level = config.get('log_level', DEFAULT_LOG_LEVEL)
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.getLevelName(log_level.upper()))
-    exporter_port = config.get('exporter_port', DEFAULT_PORT)
-    logging.debug("Config %s", config)
-    logging.info('Starting server on port %s', exporter_port)
-    start_http_server(exporter_port)
-    REGISTRY.register(JsonPathCollector(config))
-  while True: time.sleep(1)
+    parser = argparse.ArgumentParser(description='Expose metrics bu jsonpath for configured url')
+    parser.add_argument('config_file_path', help='Path of the config file')
+    args = parser.parse_args()
+    with open(args.config_file_path) as config_file:
+        config = yaml.load(config_file)
+        log_level = config.get('log_level', DEFAULT_LOG_LEVEL)
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                            level=logging.getLevelName(log_level.upper()))
+        exporter_port = config.get('exporter_port', DEFAULT_PORT)
+        logging.debug("Config %s", config)
+        logging.info('Starting server on port %s', exporter_port)
+        start_http_server(exporter_port)
+        REGISTRY.register(JsonPathCollector(config))
+    while True: time.sleep(60)
